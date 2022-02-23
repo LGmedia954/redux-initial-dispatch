6272422967ad3771eeb4082889500805ff040c1e
LGmedia954 <LeslieGonzalez@me.com>
2022-02-22 22:34:10 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,8 @@
-let state = {count: 0};
+// let state = {count: 0};
 
-function changeState(state, action){
+let state;
+
+function changeState(state = { count: 0 }, action) {
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
@@ -9,11 +11,14 @@ function changeState(state, action){
     }
   }
 
-function dispatch(action){
+  function dispatch(action){
     state = changeState(state, action)
     render()
-}
-
-function render(){
+  }
+  
+  function render(){
     document.body.textContent = state.count
-}
+  }
+  
+  dispatch({ type: '@@INIT' })
+  
